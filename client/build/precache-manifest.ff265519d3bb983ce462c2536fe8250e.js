c8042fd3e5396233cd36bdc446a8e7d628eca62c
leer <ladesigns90@gmail.com>
2020-03-10 12:02:51 -0400

    initial commit

---
+++ client/build/precache-manifest.ff265519d3bb983ce462c2536fe8250e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7092abc8d1057fbe5290e511569add4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d0f8f85e0ab292533a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "89d0f8f85e0ab292533a",
+    "url": "/static/js/main.118c4a16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
